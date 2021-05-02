eae82087cdb0f58ecfe284e643a3b23202006203
sarvarthbhatnagar <savi.sarvarth@gmail.com>
2021-05-02 19:59:31 -0400

    7:49

--- src/main/java/com/ms/api/root/msapicore/tableinfo/Welcome.java
+++ src/main/java/com/ms/api/root/msapicore/tableinfo/Welcome.java
@@ -18,7 +18,7 @@ public class Welcome {
 
     @GetMapping("/")
     public String greeting(HttpServletRequest request) {
-        //check 7:53 pm
+        //check 7:59 pm
         return "Hi! i am up at host/port : "+ request.getRequestURL().toString();
     }
 
