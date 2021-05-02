9e096ef98ef0f865d1edcd9474cff01c7dc800fb
sarvarthbhatnagar <savi.sarvarth@gmail.com>
2021-05-02 19:48:39 -0400

    commit test reinforce

--- src/main/java/com/ms/api/root/msapicore/tableinfo/Welcome.java
+++ src/main/java/com/ms/api/root/msapicore/tableinfo/Welcome.java
@@ -18,6 +18,7 @@ public class Welcome {
 
     @GetMapping("/")
     public String greeting(HttpServletRequest request) {
+        //check
         return "Hi! i am up at host/port : "+ request.getRequestURL().toString();
     }
 
